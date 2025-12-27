e66488a315796879be55dd07ce4fad2cb73bc034
Vinay Rewatkar <vinayrewatkar.257@gmail.com>
2025-12-27 16:09:16 +0530

    implement resilience4J circuit breaker

--- orderService/src/main/java/com/microservice/orderService/controller/OrderController.java
+++ orderService/src/main/java/com/microservice/orderService/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.microservice.orderService.controller;
 
 import com.microservice.orderService.dto.OrderRequestDto;
 import com.microservice.orderService.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +17,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallBack")
     public String placeOrder(@RequestBody OrderRequestDto orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order is placed";
+    }
+
+    public String fallBack(OrderRequestDto orderRequestDto, RuntimeException runtimeException){
+        return "Oops! one or moreb services failed";
     }
 }
